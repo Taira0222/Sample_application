bf54ec033b8dd757e8933351e715100fab96671e
Taira Aikawa <tir.3t.f22@gmail.com>
2025-02-05 21:31:08 +0000

    Finish pages to 459

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,19 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase) # 入力されたemailと同じデータがDB内にあるか
-    if user && user.authenticate(params[:session][:password]) # そのuserのPWが正しいか
-    # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    if user&.authenticate(params[:session][:password]) # そのuserのPWが正しいか
       reset_session #session id を更新してセッション固定を防止
       log_in user
       redirect_to user
     else
     # エラーメッセージを作成する
-      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination' 
       render 'new', status: :unprocessable_entity
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_url, status: :see_other
   end
 end
