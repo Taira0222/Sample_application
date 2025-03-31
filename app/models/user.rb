2d97a8bf964724505a36818875ba4ee9cd3ea94d
Taira Aikawa <tir.3t.f22@gmail.com>
2025-03-31 16:26:11 +0000

    Finish pages to 706

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord 
+  has_many :microposts, dependent: :destroy # ユーザーが削除されるとmicropostsも一緒に削除される
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
